69ba82214c9b9fecef9ce8a342884c32493bd579
lixu <lixu_sx@qiyi.com>
2017-09-12 14:55:11 +0800

    add example

--- DLXML.podspec
+++ DLXML.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name         = "DLXML"
-s.version      = "0.0.0"
+s.version      = "0.0.1"
   s.summary      = "DLXML"
   s.description  = <<-DESC
 	                    React description.
---
+++ Example/Example/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Example
+//
+//  Created by iqiyi on 17/9/12.
+//  Copyright © 2017年 iqiyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
